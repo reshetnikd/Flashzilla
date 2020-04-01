259e4439a2bf980fc99f219248082bd08c93f4dc
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-01 20:34:19 +0300

    Try out some code with gestures

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,8 +9,92 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var currentAmount: CGFloat = 0
+//    @State private var finalAmount: CGFloat = 1
+//    @State private var currentAmount: Angle = .degrees(0)
+//    @State private var finalAmount: Angle = .degrees(0)
+    
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+    
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+    
     var body: some View {
-        Text("Hello, World!")
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in self.offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    self.offset = .zero
+                    self.isDragging = false
+                }
+            }
+        
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    self.isDragging = true
+                }
+            }
+        
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+        return Circle()
+            .fill(Color.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+        
+        
+        
+        
+        
+//        VStack {
+//            Text("Hello, World!")
+//                .onTapGesture {
+//                    print("Text tapped")
+//                }
+//        }
+//        .simultaneousGesture(
+////        .highPriorityGesture(
+//            TapGesture()
+//                .onEnded { _ in
+//                    print("VStack tapped")
+//                }
+//        )
+//        Text("Hello, World!")
+//            .rotationEffect(currentAmount + finalAmount)
+//            .gesture(
+//                RotationGesture()
+//                    .onChanged { angle in
+//                        self.currentAmount = angle
+//                    }
+//                    .onEnded { angle in
+//                        self.finalAmount += self.currentAmount
+//                        self.currentAmount = .degrees(0)
+//                    }
+//            )
+//            .scaleEffect(finalAmount + currentAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged { amount in
+//                        self.currentAmount = amount - 1
+//                    }
+//                    .onEnded { amount in
+//                        self.finalAmount += self.currentAmount
+//                        self.currentAmount = 0
+//                    }
+//            )
+//            .onLongPressGesture(minimumDuration: 1, pressing: { inProgress in
+//                print("In progress: \(inProgress)!")
+//            }) {
+//                print("Long pressed!")
+//            }
     }
 }
 
