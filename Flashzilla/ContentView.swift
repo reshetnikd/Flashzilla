3a1a6feed1121082b83d431c73bdc85a676dc19a
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-01 21:54:04 +0300

    Try out some code with UINotificationFeedbackGenerator and Core Haptics

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
 //    @State private var currentAmount: CGFloat = 0
@@ -14,46 +15,46 @@ struct ContentView: View {
 //    @State private var currentAmount: Angle = .degrees(0)
 //    @State private var finalAmount: Angle = .degrees(0)
     
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-    
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
+//    // how far the circle has been dragged
+//    @State private var offset = CGSize.zero
+//
+//    // whether it is currently being dragged or not
+//    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in self.offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                }
-            }
-        
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-        
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
-        
-        
-        
-        
+        Text("Hello, World!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+//        // a drag gesture that updates offset and isDragging as it moves around
+//        let dragGesture = DragGesture()
+//            .onChanged { value in self.offset = value.translation }
+//            .onEnded { _ in
+//                withAnimation {
+//                    self.offset = .zero
+//                    self.isDragging = false
+//                }
+//            }
+//
+//        // a long press gesture that enables isDragging
+//        let pressGesture = LongPressGesture()
+//            .onEnded { value in
+//                withAnimation {
+//                    self.isDragging = true
+//                }
+//            }
+//
+//        // a combined gesture that forces the user to long press then drag
+//        let combined = pressGesture.sequenced(before: dragGesture)
+//
+//        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+//        return Circle()
+//            .fill(Color.red)
+//            .frame(width: 64, height: 64)
+//            .scaleEffect(isDragging ? 1.5 : 1)
+//            .offset(offset)
+//            .gesture(combined)
+//        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //        VStack {
 //            Text("Hello, World!")
 //                .onTapGesture {
@@ -95,6 +96,56 @@ struct ContentView: View {
 //            }) {
 //                print("Long pressed!")
 //            }
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func complexSuccess() {
+        // make sure that the divice supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+//        events.append(event)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription).")
+        }
     }
 }
 
